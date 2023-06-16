fa044c4d0ed3250f7b0c586923cedcaa25bfaeba
snkos <snkos@DESKTOP-LIO1E5U>
2023-06-16 15:53:37 +0200

    Add project files.

---
+++ Models/Food.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace WebApplication3.Models
+{
+    public class Food
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int FoodId { get; set; }
+        public int CusId { get; set; }
+        public  bool Pizza { get; set; }
+        public  bool Pasta { get; set; }
+        public bool PapWors { get; set; }
+        public bool  { get; set; }
+        public bool PapWors { get; set; }
+
+
+
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication3.Models
+{
+    public class Person
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DisplayName("Event Code")]
+        public int CusId { get; set; }
+        [DisplayName("Surname")]
+        public string Surname { get; set; }
+        [DisplayName("Firt Name")]
+        public string  Fname { get; set; }
+        [DisplayName("Contact Number")]
+        public string ContactNo { get; set; }
+        public DateTime Date { get; set; }
+        [DisplayName("Person's age")]
+        public int Age { get; set; }
+
+
+    }
+}
